88bbed70cedfb0ee863776fc40456bdc768e50a8
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-23 15:02:18 +0100

    ex01 Defining the constructor function usling class syntax

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,13 +1,13 @@
 //Only change code below this line
 class Car {
-    constructor(manufacturer, model, production){
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.production = production;
-    }
+  constructor(manufacturer, model, production) {
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.production = production;
+  }
 }
+//Only change code above this line
 
-//Only change code below this line
 const bmw = new Car("BMW", "BMW X3", 2003);
 console.log(bmw.manufacturer);
 console.log(bmw.model);
