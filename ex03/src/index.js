37c2a20226c400bb61dbe5f7ed40b4c3a1355aaf
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-23 12:06:47 +0100

    ex03 Static keyword

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+//Only change code below this line
+class Person {
+    
+    static display(){
+       console.log("Static method is invoked from Person class");
+    }
+    show() {
+        Person.display();
+    }
+}
+//Only change code below this line
+var message = new Person();
+message.show();
+module.exports = Person;
