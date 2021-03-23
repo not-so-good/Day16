4e988ed7b1e3b76f5efcc9709620673d1552f8ab
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-23 15:02:32 +0100

    ex02 Getters and setters - Controling the access to an object

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,17 +1,17 @@
 //Only change code below this line
 class Book {
-    constructor(title){
-        this.title = title;
-    }
-    get title(){
-        return this._title;
-    }
-    set title(newTitle){
-        this._title = newTitle;
-    }
+  constructor(title) {
+    this.title = title;
+  }
+  get title() {
+    return this._title;
+  }
+  set title(newTitle) {
+    this._title = newTitle;
+  }
 }
+//Only change code above this line
 
-//Only change code below this line
 const littlePrince = new Book("The Little Prince"); //Change this line
 console.log(littlePrince.title); //The little Prince
 littlePrince.title = "Le Petit Prince"; //Change this line
